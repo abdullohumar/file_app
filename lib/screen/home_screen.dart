90872d9720a69e235429a56d00f2a0de47ae69cf
Abdulloh Umar <iniautok@gmail.com>
2025-05-21 16:59:17 +0700

    read dan write action

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:file_app/provider/file_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -18,6 +20,37 @@ class _HomeScreenState extends State<HomeScreen> {
     _fileNameController.dispose();
     _contentsController.dispose();
     super.dispose();
+  }
+
+  Future<String?> openListOfFileName() async {
+    final fileProvider = context.read<FileProvider>();
+    await fileProvider.getAllFilesInDirectory();
+    final listOfFile = fileProvider.filesName;
+
+    String? result;
+    if (mounted) {
+      result = await showDialog(
+        context: context,
+        builder: (context) {
+          return SimpleDialog(
+            title: const Text('Select a file'),
+            children:
+                listOfFile
+                    .map(
+                      (file) => SimpleDialogOption(
+                        onPressed: () {
+                          Navigator.pop(context, file);
+                        },
+                        child: Text(file),
+                      ),
+                    )
+                    .toList(),
+          );
+        },
+      );
+      return result;
+    }
+    return null;
   }
 
   @override
@@ -53,13 +86,13 @@ class _HomeScreenState extends State<HomeScreen> {
                       child: Text('Save File', style: _textStyle),
                     ),
                   ),
-                  Expanded(child: TextField(
-                    controller: _fileNameController,
-                    style: _textStyle,
-                    decoration: InputDecoration(
-                      hintText: 'file_name.txt',
+                  Expanded(
+                    child: TextField(
+                      controller: _fileNameController,
+                      style: _textStyle,
+                      decoration: InputDecoration(hintText: 'file_name.txt'),
                     ),
-                  )),
+                  ),
                 ],
               ),
               const SizedBox(height: 20),
@@ -70,9 +103,48 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
-  void _createNewFile() {}
+  void showSnackBar(String text) {
+    final scaffoldMessenger = ScaffoldMessenger.of(context);
 
-  void _openFile() {}
+    scaffoldMessenger.showSnackBar(SnackBar(content: Text(text)));
+  }
 
-  void _saveFile() {}
+  void _createNewFile() {
+    _fileNameController.clear();
+    _contentsController.clear();
+    showSnackBar("New file created");
+  }
+
+  void _saveFile() async {
+    final fileName = _fileNameController.text;
+    final contents = _contentsController.text;
+
+    if (fileName.isEmpty || contents.isEmpty) {
+      showSnackBar("Please enter a file name and contents");
+      return;
+    }
+
+    final fileProvider = context.read<FileProvider>();
+    await fileProvider
+        .saveFile(fileName, contents)
+        .then((value) => showSnackBar(fileProvider.message))
+        .catchError((error) => showSnackBar(fileProvider.message))
+        .whenComplete(() {
+          _fileNameController.clear();
+          _contentsController.clear();
+        });
+  }
+
+  void _openFile() async {
+    final fileProvider = context.read<FileProvider>();
+    final fileName = (await openListOfFileName()) ?? "";
+
+    if (fileName.isNotEmpty) {
+      await fileProvider.readFile(fileName);
+
+      _fileNameController.text = fileName;
+      _contentsController.text = fileProvider.contents ?? "";
+      showSnackBar(fileProvider.message);
+    }
+  }
 }
